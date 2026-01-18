131e09b20be3ff4cbe71ad7b18a4b7a72e4e6c32
ashtikar86 <swapnil.whatsappbkp@gmail.com>
2026-01-18 19:10:01 +0530

    created docker files

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.102:5000/api',
+    baseURL: '/api', // Relative path for Nginx Proxy
 });
 
 api.interceptors.request.use((config) => {
